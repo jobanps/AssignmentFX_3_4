4d55b11fbc1cf75812d88ad524c57e81f03fc99e
Joban <Joban@192.168.0.23>
2019-07-20 17:10:58 -0400

    Testing Pending, All Completed

--- src/AssignmentFX.java
+++ src/AssignmentFX.java
@@ -1,10 +1,15 @@
+import java.text.DecimalFormat;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -19,11 +24,10 @@ public class AssignmentFX extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		// Define UI Controls
 		Label headLbl = new Label("Airport Ride Calculator");
-		headLbl.setAlignment(Pos.CENTER);
 		headLbl.setUnderline(true);
 		
 		Label descLbl = new Label("From: ");
-		headLbl.setAlignment(Pos.CENTER_LEFT);
+		//headLbl.setAlignment(Pos.CENTER_LEFT);
 		
 		ComboBox<String> locationList = new ComboBox<String>();
 		locationList.getItems().add("Cestar College");
@@ -34,7 +38,7 @@ public class AssignmentFX extends Application{
 		CheckBox etrChkBox = new CheckBox("Use 407 ETR?");
 		CheckBox tipChkBox = new CheckBox("Add Tip?");
 		
-		Button calcButton = new Button("Calculate");
+		Button calcButton = new Button("CALCULATE");
 		
 		Label resultLbl = new Label("");
 		
@@ -45,26 +49,89 @@ public class AssignmentFX extends Application{
 		hRoot.getChildren().add(locationList);
 		hRoot.setAlignment(Pos.CENTER);
 		
+		
+		//
+		calcButton.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override
+		    public void handle(ActionEvent e) {
+		        // Logic to calculate Ride Fare when clicked on Calculate
+		    	String location = locationList.getSelectionModel().getSelectedItem();
+		    	boolean isValidLocation = true;
+		    	double etrDistanceBrampton = 12;
+		    	double etrDistanceCestar = 15;
+		    	double tripCost = 0;
+		    	
+		    	if (("Cestar College").equals(location)) {
+		    		
+		    		tripCost = 51;
+		    		
+		    		if(etrChkBox.isSelected()) { 
+		    			tripCost += etrDistanceCestar * 0.25;
+		    			etrChkBox.setText("Use 407 ETR? (" + etrDistanceCestar + "kms )");
+		    		}
+		    		
+		    	} else if (("Brampton").equals(location)) {
+		    	
+		    		tripCost = 38;
+		    		
+		    		if(etrChkBox.isSelected()) { 
+		    			tripCost += etrDistanceBrampton * 0.25;
+		    			etrChkBox.setText("Use 407 ETR? (" + etrDistanceBrampton + "kms )");
+		    		}
+		    		
+		    	} else { 
+		    		isValidLocation = false;
+		    		resultLbl.setText("Please select location !");
+		    	}
+		    	
+		    	if(isValidLocation){
+		    	if(extraluggageChkBox.isSelected()) 
+	    			tripCost += 10;
+		    	if(petsChkBox.isSelected()) 
+	    			tripCost += 6;
+		    	
+		    	//Calculating Tax
+		    	tripCost *= 1.13;
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	if(tipChkBox.isSelected()) 
+	    			tripCost *= 1.15;
+		    	
+		    	DecimalFormat deciFormat = new DecimalFormat("###.##");
+		    	double tripCostWithPrecision = Double.parseDouble(deciFormat.format(tripCost));
+		    	resultLbl.setText("The total fare is: " + tripCostWithPrecision);
+		    }
+		    }
+		});
+		
+		GridPane gRoot = new GridPane();
+		gRoot.setVgap(15);
+		gRoot.addRow(0, extraluggageChkBox);
+		gRoot.addRow(1, petsChkBox);
+		gRoot.addRow(2, etrChkBox);
+		gRoot.addRow(3, tipChkBox);
+		gRoot.setAlignment(Pos.CENTER);
+		
 		VBox vRoot = new VBox();
-		vRoot.setSpacing(12);
+		vRoot.setSpacing(15);
 		vRoot.getChildren().add(headLbl);
-		vRoot.getChildren().addAll(hRoot);
-		vRoot.getChildren().add(extraluggageChkBox);
-		vRoot.getChildren().add(petsChkBox);
-		vRoot.getChildren().add(etrChkBox);
-		vRoot.getChildren().add(tipChkBox);
+		vRoot.getChildren().add(hRoot);
+		vRoot.getChildren().add(gRoot);
 		vRoot.getChildren().add(calcButton);
 		vRoot.getChildren().add(resultLbl);
 		
 		vRoot.setAlignment(Pos.CENTER);
 		
 		//Creating Scene
-		Scene scene = new Scene(vRoot,250,400);
+		Scene scene = new Scene(vRoot,300,350);
 	
 		
 		//creating stage
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("Assignment_3_4");
+		primaryStage.setTitle("AirRideLimo.com");
 		primaryStage.show();
 		
 	}
